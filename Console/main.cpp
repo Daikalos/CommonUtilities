d44e8ed1fde7a9631f29a9081c66199e6e341696
Tobias Garpenhall <tobias.garpenhall2000@gmail.com>
2024-04-02 13:22:49 +0200

    fix

--- Console/main.cpp
+++ Console/main.cpp
@@ -3,16 +3,25 @@
 
 #include <CommonUtilities/Algorithms/Sort.hpp>
 #include <CommonUtilities/Utility/Random.hpp>
+#include <CommonUtilities/Utility/Benchmark.h>
 
 int main()
 {
-	std::vector<int> test = cu::rn::RandomVector(100);
-	std::vector<int> test2 = cu::rn::RandomVector(100);
-	std::vector<int> test3 = cu::rn::RandomVector(100);
+	std::vector<int> test = cu::rn::RandomVector(100000000);
+	std::vector<int> test2 = cu::rn::RandomVector(100000000);
+	std::vector<int> test3 = cu::rn::RandomVector(100000000);
 
-	CommonUtilities::QuickSort(test);
+	cu::bm::Begin();
+	CommonUtilities::MergeSort(test);
+	cu::bm::End();
+
+	cu::bm::Begin();
 	CommonUtilities::QuickSort(test2);
-	CommonUtilities::QuickSort(test3);
+	cu::bm::End();
+
+	cu::bm::Begin();
+	CommonUtilities::SelectionSort(test3);
+	cu::bm::End();
 
 	return 0;
 }

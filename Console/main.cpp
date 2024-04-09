0a3831ff64f49b0acdf0ee678ad84ed56b286963
Tobias Garpenhall <tobias.garpenhall2000@gmail.com>
2024-04-09 12:14:44 +0200

    map

--- Console/main.cpp
+++ Console/main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 #include <string>
 
-#include <CommonUtilities/Structures/Blackboard.hpp>
-#include <CommonUtilities/Utility/Random.hpp>
-#include <CommonUtilities/Utility/Benchmark.h>
+#include <CommonUtilities/Structures/Misc/HashMap.hpp>
 
 int main()
 {
-	cu::Blackboard blackboard;
-	blackboard.Set("hey", 5);
-	int yes = blackboard.Get<int>("hey");
+	CommonUtilities::HashMap<std::string, int> map(8);
+	map.Insert("hello", 5);
+	map.Insert("test", 12);
+	map.Insert("yes", 14);
+	map.Insert("wow", 18);
+	map.Insert("hey", 54);
+	map.Insert("jesus", -43);
+	map.Insert("what", 3);
+	map.Insert("true", 32);
+	map.Insert("jesus", 50);
+	int val = *map.Get("hello");
+	int val2 = *map.Get("test");
+	int val3 = *map.Get("yes");
+	int val4 = *map.Get("wow");
+	int val5 = *map.Get("hey");
+	int val6 = *map.Get("jesus");
+	int val7 = *map.Get("what");
+	int val8 = *map.Get("true");
 	return 0;
 }

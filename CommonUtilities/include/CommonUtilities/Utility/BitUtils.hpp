4a2b657c90c22a8c5110689d24a2cd202b49c9fb
Tobias Garpenhall <tobias.garpenhall2000@gmail.com>
2024-08-24 21:17:50 +0200

    Update BitUtils.hpp

--- CommonUtilities/include/CommonUtilities/Utility/BitUtils.hpp
+++ CommonUtilities/include/CommonUtilities/Utility/BitUtils.hpp
@@ -11,15 +11,25 @@ namespace CommonUtilities
 	template<std::uint64_t... BitSizes> requires ((BitSizes + ...) == sizeof(std::uint64_t) * CHAR_BIT)
 	constexpr std::uint64_t PackValues64(const std::array<std::uint64_t, sizeof...(BitSizes)>& aValuesToPack)
 	{
-		constexpr std::array<std::uint64_t, sizeof...(BitSizes)> BitArr{ BitSizes... };
+		constexpr auto ComputeDisplacements = []<std::uint64_t... BitSizes>() constexpr
+		{
+			constexpr std::array<std::uint64_t, sizeof...(BitSizes)> bitArr{ BitSizes... };
+			std::array<std::uint64_t, sizeof...(BitSizes)> bitDisplacements{};
+
+			for (std::uint64_t i = 1; i < sizeof...(BitSizes); ++i)
+				bitDisplacements[i] += bitDisplacements[i - 1] + bitArr[i - 1];
+
+			return bitDisplacements;
+		};
+
+		constexpr std::array<std::uint64_t, sizeof...(BitSizes)> bitArr { BitSizes... };
+		constexpr std::array<std::uint64_t, sizeof...(BitSizes)> bitDisplacementArr = ComputeDisplacements.template operator()<BitSizes...>();
 
 		std::uint64_t result{};
 
-		std::uint64_t bitDisplacement{};
 		for (std::uint64_t i = 0; i < sizeof...(BitSizes); ++i)
 		{
-			result |= aValuesToPack[i] << uint64_t(sizeof(std::uint64_t) * CHAR_BIT - (BitArr[i] + bitDisplacement));
-			bitDisplacement += BitArr[i];
+			result |= aValuesToPack[i] << uint64_t(sizeof(std::uint64_t) * CHAR_BIT - (bitArr[i] + bitDisplacementArr[i]));
 		}
 
 		return result;
